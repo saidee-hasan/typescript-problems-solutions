afe2140e3a462ab99bafad0d2c447bab33241abc
Saidee Hasan <mdsaideehasan@gmail.com>
2025-05-08 17:59:58 +0600

    interface

--- src/problem.ts
+++ src/problem.ts
@@ -70,5 +70,28 @@ function processValue(value: string | number): number {
     return value * 2;  
   }
 }
-console.log(processValue("hello")); 
-console.log(processValue(10));       
+// console.log(processValue("hello")); 
+// console.log(processValue(10));       
+
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products.length === 0) return null;
+
+  return products.reduce((maxProduct, currentProduct) => {
+    return currentProduct.price > maxProduct.price ? currentProduct : maxProduct;
+  });
+}
+
+
+const products = [
+  { name: "Pen", price: 10 },
+  { name: "Notebook", price: 25 },
+  { name: "Bag", price: 50 }
+];
+
+// console.log(getMostExpensiveProduct(products));  
