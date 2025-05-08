7c07131c5f96f01f4af36f6e3543e9f4d6b11ac0
Saidee Hasan <mdsaideehasan@gmail.com>
2025-05-08 16:59:40 +0600

    filterByRating

--- dist/problem.js
+++ dist/problem.js
@@ -2,6 +2,15 @@
 function formatString(input, toUpper = true) {
     return toUpper ? input.toUpperCase() : input.toLocaleLowerCase();
 }
-console.log(formatString("Hello", false));
-console.log(formatString("Hello", true));
-console.log(formatString("Hello", false));
+// console.log(formatString("Hello"));
+// console.log(formatString("Hello", true));
+// console.log(formatString("Hello", false));
+function filterByRating(items) {
+    return items.filter(item => item.rating >= 4);
+}
+const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+];
+console.log(filterByRating(books));
--- src/problem.ts
+++ src/problem.ts
@@ -5,3 +5,17 @@ function formatString(input:string ,toUpper:boolean =true):string{
 // console.log(formatString("Hello"));
 // console.log(formatString("Hello", true));
 // console.log(formatString("Hello", false));
+
+
+
+function filterByRating(items:{title:string; rating:number}[]):{title:string; rating:number}[]{
+  return items.filter(item => item.rating >= 4);
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 }
+];
+
+console.log(filterByRating(books)); 
