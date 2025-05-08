f246e8b0fc0bc160d5c956b24f5c895ec40b8cc0
Saidee Hasan <mdsaideehasan@gmail.com>
2025-05-08 18:16:20 +0600

    enum Day

--- dist/problem.js
+++ dist/problem.js
@@ -48,5 +48,38 @@ function processValue(value) {
         return value * 2;
     }
 }
-console.log(processValue("hello"));
-console.log(processValue(100));
+function getMostExpensiveProduct(products) {
+    if (products.length === 0)
+        return null;
+    return products.reduce((maxProduct, currentProduct) => {
+        return currentProduct.price > maxProduct.price ? currentProduct : maxProduct;
+    });
+}
+const products = [
+    { name: "Pen", price: 10 },
+    { name: "Notebook", price: 25 },
+    { name: "Bag", price: 50 }
+];
+// console.log(getMostExpensiveProduct(products));  
+var Day;
+(function (Day) {
+    Day[Day["Monday"] = 0] = "Monday";
+    Day[Day["Tuesday"] = 1] = "Tuesday";
+    Day[Day["Wednesday"] = 2] = "Wednesday";
+    Day[Day["Thursday"] = 3] = "Thursday";
+    Day[Day["Friday"] = 4] = "Friday";
+    Day[Day["Saturday"] = 5] = "Saturday";
+    Day[Day["Sunday"] = 6] = "Sunday";
+})(Day || (Day = {}));
+function getDayType(day) {
+    switch (day) {
+        case Day.Saturday:
+        case Day.Sunday:
+            return "Weekend";
+        default:
+            return "Weekday";
+    }
+}
+// Example Usage:
+console.log(getDayType(Day.Monday)); // Output: "Weekday"
+console.log(getDayType(Day.Sunday)); // Output: "Weekend"
