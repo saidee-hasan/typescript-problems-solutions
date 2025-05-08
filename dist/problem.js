dd6024100a8f2c1f44c2ffdf37b90572ad0425d3
Saidee Hasan <mdsaideehasan@gmail.com>
2025-05-08 17:06:37 +0600

    class Vehicle

--- dist/problem.js
+++ dist/problem.js
@@ -17,5 +17,27 @@ const books = [
 function concatenateArrays(...arrays) {
     return arrays.flat();
 }
-console.log(concatenateArrays(["a", "b"], ["c"]));
-console.log(concatenateArrays([1, 2], [3, 4], [5]));
+// console.log(concatenateArrays(["a", "b"], ["c"]));       
+// console.log(concatenateArrays([1, 2], [3, 4], [5]));  
+class Vehicle {
+    constructor(make, year) {
+        this.make = make;
+        this.year = year;
+    }
+    getInfo() {
+        return `Make: ${this.make}, Year: ${this.year}`;
+    }
+}
+class Car extends Vehicle {
+    constructor(make, year, model) {
+        super(make, year); // Calling the parent class constructor
+        this.model = model;
+    }
+    getModel() {
+        return `Model: ${this.model}`;
+    }
+}
+// Example usage:
+const myCar = new Car("Toyota", 2020, "Corolla");
+console.log(myCar.getInfo()); // Output: "Make: Toyota, Year: 2020"
+console.log(myCar.getModel()); // Output: "Model: Corolla"
